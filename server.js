b572dd5e567d07733437c239b63676ba82ed33ad
Muhammad Ali <muhammadaliayaan295@gmail.com>
2024-09-08 14:10:34 +0500

    err corr 1.4

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const resumeRoutes = require('./routes'); // Adjust path if needed
 
 const app = express();
 const port = 3000;
 
 // Use the cors middleware with specific origin
-app.use(cors({
-    origin: 'https://resume-builder-2qwij5y0u-muhammad-alis-projects-5543e715.vercel.app'
-}));
+// app.use(cors({
+//     origin: 'https://resume-builder-2qwij5y0u-muhammad-alis-projects-5543e715.vercel.app'
+// }));
 
 app.use(express.json()); // Use built-in express.json() middleware
 app.use('/api', resumeRoutes);
