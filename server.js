caae7ec44d521bb400f92b95c1f2743c2ac10d09
Muhammad Ali <muhammadaliayaan295@gmail.com>
2024-09-08 14:26:44 +0500

    err 1.7

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const port = 3000;
 
 // Use the cors middleware with specific origin
 app.use(cors({
-    origin: 'https://resume-builder-2qwij5y0u-muhammad-alis-projects-5543e715.vercel.app'
+    origin: 'https://resume-builder-2qwij5y0u-muhammad-alis-projects-5543e715.vercel.app/'
 }));
 
 app.use(express.json()); // Use built-in express.json() middleware
 app.use('/api', resumeRoutes);
 
 app.listen(port, () => {
-    console.log(`Server running at https://resume-builder-2qwij5y0u-muhammad-alis-projects-5543e715.vercel.app`);
+    console.log(`Server running at https://resume-builder-2qwij5y0u-muhammad-alis-projects-5543e715.vercel.app/`);
 });
